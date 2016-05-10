ad7d2008ffad7552e27eae272a870e9ab098e443
xiancong.lxc <xiancong.lxc@alibaba-inc.com>
2016-05-10 10:18:05 +0800

    fix(manifest.json): 符合最新的chrome extension api

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var	bm = chrome.benchmarking,
-	tabs = chrome.tabs,
-	optUrl = 'dns-flusher-options.html';
+    tabs = chrome.tabs,
+    optUrl = 'dns-flusher-options.html';
 
 if(undefined == bm){ // benchmarking disabled
   tabs.create({url: optUrl});
@@ -8,41 +8,45 @@ if(undefined == bm){ // benchmarking disabled
 
 function main(url) {
   if(undefined == bm){ // benchmarking disabled
-		tabs.query({url: 'chrome-extension://*/' + optUrl}, function(tabArr){
-			if(tabArr.length > 0){
-				tabs.update(tabArr[0].id, {active: true});	
-			}else{
-				tabs.create({url: optUrl});
-			}
-		});
-		return;
-	} else {
+        tabs.query({url: 'chrome-extension://*/' + optUrl}, function(tabArr){
+            if(tabArr.length > 0){
+                tabs.update(tabArr[0].id, {active: true});
+            }else{
+                tabs.create({url: optUrl});
+            }
+        });
+        return;
+    } else {
     // benchmarking enabled
     bm.closeConnections();
     bm.clearHostResolverCache();
+    // chrome.storage.sync.set('reload', {
+    //     reload: true
+    // }); // TODO
   }
   chrome.storage.sync.get('reload', function(items) {
-	  if(items.reload) tabs.query({
+    tabs.query({
       active: true,
       currentWindow: true
     }, function(_tabs) {
       var tab = _tabs[0];
       if (url && url.trim() !== '') {
         if (!/^http/.test(url)) {
-          url = 'http://' + url; 
+          url = 'http://' + url;
         }
+
         tabs.executeScript({
           code: 'window.location.href="' + url + '"'
-        })
+        });
       } else {
-        tabs.reload(tab.id) 
+        tabs.reload(tab.id);
       }
     });
   });
 }
 
 chrome.browserAction.onClicked.addListener(function(tab){
-	main();
+    main();
 });
 
 chrome.omnibox.onInputEntered.addListener(function(url, suggest) {
