b8c0748dd0a68a020cb17d762cbf26945b725e3a
xiancong.lxc <xiancong.lxc@alibaba-inc.com>
2016-05-17 15:51:58 +0800

    feat: ɾ³ýreloadѡÏî; ¸ü¸Äͼ±ê; ÓŻ¯´úÂë

--- background.js
+++ background.js
@@ -1,54 +1,27 @@
 var	bm = chrome.benchmarking,
-    tabs = chrome.tabs,
-    optUrl = 'dns-flusher-options.html';
-
-if(undefined == bm){ // benchmarking disabled
-  tabs.create({url: optUrl});
-}
+  tabs = chrome.tabs,
+  optUrl = 'dns-flusher-options.html';
 
 function main(url) {
   if(undefined == bm){ // benchmarking disabled
-        tabs.query({url: 'chrome-extension://*/' + optUrl}, function(tabArr){
-            if(tabArr.length > 0){
-                tabs.update(tabArr[0].id, {active: true});
-            }else{
-                tabs.create({url: optUrl});
-            }
-        });
-        return;
-    } else {
-    // benchmarking enabled
-    bm.closeConnections();
-    bm.clearHostResolverCache();
-    // chrome.storage.sync.set('reload', {
-    //     reload: true
-    // }); // TODO
+    tabs.create({url: optUrl});
   }
-  chrome.storage.sync.get('reload', function(items) {
-    tabs.query({
-      active: true,
-      currentWindow: true
-    }, function(_tabs) {
-      var tab = _tabs[0];
-      if (url && url.trim() !== '') {
-        if (!/^http/.test(url)) {
-          url = 'http://' + url;
-        }
-
-        tabs.executeScript({
-          code: 'window.location.href="' + url + '"'
-        });
-      } else {
-        tabs.reload(tab.id);
-      }
+  bm.closeConnections();
+  bm.clearHostResolverCache();
+  if (url && url.trim() !== '') {
+    if (!/^http/.test(url)) {
+      url = 'http://' + url;
+    }
+    tabs.update(null, {
+      url: url
     });
-  });
+  } else {
+    tabs.reload();
+  }
 }
 
-chrome.browserAction.onClicked.addListener(function(tab){
-    main();
-});
-
-chrome.omnibox.onInputEntered.addListener(function(url, suggest) {
-  main(url);
-});
+function browserActionClicked() {
+  main();
+}
+chrome.browserAction.onClicked.addListener(browserActionClicked);
+chrome.omnibox.onInputEntered.addListener(main);
--- dns-flusher-options.js
+++ dns-flusher-options.js
@@ -2,44 +2,20 @@ var _m = chrome.i18n.getMessage
   store = chrome.storage.sync;
 
 function $(id){
-	return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function setMsg(id){
-	$(id).innerHTML = _m(id);
+  $(id).innerHTML = _m(id);
 }
 
 function initI18N(){
-	document.title = _m('optionPageTitle') + ' - DNS Flusher for Chrome';
-	setMsg('optionName');
-	setMsg('optionLabel');
-	setMsg('optionDescription');
-	setMsg('attentionName');
-	setMsg('attentionDescription');
+  document.title = _m('optionPageTitle') + ' - DNS Flusher for Chrome';
+  setMsg('attentionName');
+  setMsg('attentionDescription');
   setMsg('addressBarReloadTips');
-	// setMsg('ffOption');
-}
-
-function initReloadUI(){
-	var el = $('reload-setting');
-
-  store.get('reload', function(items) {
-    if (null == items.reload) { // reload没有设置过, 那么就设置reload为true
-      reload = true;
-      store.set({reload: reload}, function() {
-	      el.checked = reload;
-      });
-    } else {
-	      el.checked = items.reload;
-    }
-  });
-
-	el.onclick = function(){
-		store.set({reload: el.checked});
-	}
 }
 
 initI18N();
-initReloadUI();
 
 
